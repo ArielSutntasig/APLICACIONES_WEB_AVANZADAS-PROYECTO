1c8cc78b665945ce7c8532a8e942f53cd83abdbc
Ariel Suntasig <139640206+ArielSutntasig@users.noreply.github.com>
2024-12-12 11:41:37 -0500

    Fix: update redirection from login.html to index.html in multiple files; remove unused counter.js and main.js

--- Backend/run.py
+++ Backend/run.py
@@ -3,5 +3,5 @@ from app import create_app, socketio  # Importa socketio desde tu __init__.py
 app = create_app()
 
 if __name__ == "__main__":
-    # Cambia app.run por socketio.run para manejar SocketIO
+    # socketio.run para manejar SocketIO
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
